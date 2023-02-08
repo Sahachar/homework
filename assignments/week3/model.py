cdbe613f244ac689b4bedf836e16e6e2fd011131
Sahachar <reddysahachar@gmail>
2023-02-08 06:03:39 -0500

    Submit_w3_1

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,8 +1,13 @@
 import torch
 from typing import Callable
+import torch.nn as nn
 
 
-class MLP:
+class MLP(nn.Module):
+    """
+    MLP class implementation
+    """
+
     def __init__(
         self,
         input_size: int,
@@ -22,9 +27,24 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        self.hidden_count = hidden_count
+        self.activation = torch.nn.functional.relu
+        self.initializer = initializer
 
-    def forward(self, x):
+        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.bn_1 = torch.nn.BatchNorm1d(hidden_size)
+        self.fc2 = torch.nn.Linear(self.hidden_size, self.num_classes)
+
+        # self.initializer(self.fc1.weight)
+        # self.initializer(self.fc2.weight)
+
+        # self.parameters = nn.ModuleList([self.fc1, self.fc2, self.bn_1]).parameters()
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
         """
         Forward pass of the network.
 
@@ -34,4 +54,6 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        x = self.activation(self.bn_1(self.fc1(x)))
+        x = self.fc2(x)
+        return x
