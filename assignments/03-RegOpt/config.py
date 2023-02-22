6205c720ca7516974cb4f312a1e5eab60b37b7c9
Sahachar <reddysahachar@gmail>
2023-02-21 21:31:11 -0500

    Submit_HW3_01

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,23 +8,20 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
 
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 7
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
-    lrs_kwargs = {
-        # You can pass arguments to the learning rate scheduler
-        # constructor here.
-    }
+    lrs_kwargs = {"T_max": 448, "eta_min": 0, "last_epoch": -1, "verbose": False}
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
-        [
-            ToTensor(),
-        ]
+        [ToTensor(), Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,24 +1,67 @@
 from typing import List
-
+import warnings
 from torch.optim.lr_scheduler import _LRScheduler
+import math
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    The class schedules the Learning Rate
+    """
+
+    def __init__(self, optimizer, T_max, eta_min=0, last_epoch=-1, verbose=False):
+        self.T_max = T_max
+        self.eta_min = eta_min
+        super(CustomLRScheduler, self).__init__(optimizer, last_epoch, verbose)
+
+    def get_lr(self) -> None:
         """
-        Create a new scheduler.
-
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
-
+        Gets the last learning rate computed by the scheduler
         """
-        # ... Your Code Here ...
-        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        if not self._get_lr_called_within_step:
+            warnings.warn(
+                "To get the last learning rate computed by the scheduler, "
+                "please use `get_last_lr()`.",
+                UserWarning,
+            )
 
-    def get_lr(self) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
+        if self.last_epoch == 0:
+            return [group["lr"] for group in self.optimizer.param_groups]
+        elif self._step_count == 1 and self.last_epoch > 0:
+            return [
+                self.eta_min
+                + (base_lr - self.eta_min)
+                * (1 + math.cos((self.last_epoch) * math.pi / self.T_max))
+                / 2
+                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+            ]
+        elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max) == 0:
+            return [
+                group["lr"]
+                + (base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
+                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+            ]
+        return [
+            (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+            / (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max))
+            * (group["lr"] - self.eta_min)
+            + self.eta_min
+            for group in self.optimizer.param_groups
+        ]
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+    def _get_closed_form_lr(self):
+        return [
+            self.eta_min
+            + (base_lr - self.eta_min)
+            * (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+            / 2
+            for base_lr in self.base_lrs
+        ]
+
+    # def get_lr(self) -> List[float]:
+    #     # Note to students: You CANNOT change the arguments or return type of
+    #     # this function (because it is called internally by Torch)
+
+    #     # ... Your Code Here ...
+    #     # Here's our dumb baseline implementation:
+    #     return [i for i in self.base_lrs]
