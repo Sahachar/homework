aa98a3ea4c1f98637c2e5ff54592b520b5290b28
Sahachar <reddysahachar@gmail>
2023-02-21 21:55:33 -0500

    Submit_HW3_03

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -15,20 +15,18 @@ class CONFIG:
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "T_max": 448,
-        "eta_min": 0,
-        "last_epoch": -1,
-        "verbose": False,
+        "T_max": 448, "eta_min": 0, "last_epoch": -1, "verbose": False,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(),
-        lr=CONFIG.initial_learning_rate,
-        weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
-        [ToTensor(), Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])]
+        [
+            ToTensor(), Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])
+            
+        ]
     )
